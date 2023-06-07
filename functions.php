a6a7a38de4d1f252bf93c5264ce7219b548afa88
TijaniTatu <riekotijani@gmail.com>
2023-06-08 00:56:35 +0300

    first commit

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+
+function selectDataFromDatabase($servername, $username, $password, $database, $tableName)
+{
+    require_once("database.php");
+
+    // SQL query to select data
+    $sql = "SELECT * FROM  .Patients";
+
+    // Execute the query
+    $result = $conn->query($sql);
+
+    // Check if any rows are returned
+    if ($result->num_rows > 0) {
+        // Create an array to hold the data
+        $data = array();
+
+        // Loop through each row
+        while ($row = $result->fetch_assoc()) {
+            // Add the row to the data array
+            $data[] = $row;
+        }
+
+        // Close the connection
+        $conn->close();
+
+        // Return the retrieved data
+        return $data;
+    } else {
+        // Close the connection
+        $conn->close();
+
+        // Return an empty array if no data found
+        return array();
+    }
+}
+// Call the function and assign the returned data to a variable
+$data = selectDataFromDatabase("localhost", "root", "","db_tijani_tatu_150397", "Patients");
+
+// Display or process the retrieved data
+foreach ($data as $row) {
+    print_r($row);
+}
+?>
