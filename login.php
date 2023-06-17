c4aa923c0f4f953a2be9ca7a20eca7cf165d14bc
TijaniTatu <riekotijani@gmail.com>
2023-06-18 01:15:39 +0300

    combobox database pcker

--- login.php
+++ login.php
@@ -5,12 +5,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve the submitted ID and password
     $user_name = $_POST["user_name"];
     $password = $_POST["password"];
-
+    $selectedTable= $_POST["firstComboBox"];
     // Further validation or sanitization can be applied if necessary
-
+ 
+    // Prepare the query based on the selected table
+  switch ($selectedTable) {
+    case "patient":
+        $tableName = "Patients";
+        break;
+    case "doctor":
+        $tableName = "Doctors";
+        break;
+    case "pharmacy":
+        $tableName = "Pharmacy";
+        break;
+    default:
+        $tableName = "";
+        break;
+}
     // Prepare the query
-    $stmt = $conn->prepare("SELECT user_name FROM Patients WHERE user_name = ? AND password = ?");
-    $stmt->bind_Param("ss",$user_name,$password);
+    if (!empty($tableName)) {
+        // Prepare the query
+        $stmt = $conn->prepare("SELECT user_name FROM $tableName WHERE user_name = ? AND password = ?");
+        $stmt->bind_param("ss", $user_name, $password);
+        
     
     // Execute the query
     $stmt->execute();
@@ -21,11 +39,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if a matching record is found
     if ($stmt->fetch()) {
         // Successful login, grant access
-        echo "<br>"."Welcome patient " . $user_name;
+        echo "<br>" . "Welcome $selectedTable " . $user_name;
     } else {
         // Invalid credentials, deny access
-        echo "<br>"."Invalid credentials. Please try again.";
+        echo "<br>" . "Invalid credentials. Please try again.";
     }
+} else {
+    // No table selected, display an error message
+    echo "<br>" . "Please select a valid option.";
 }
+} else {
+// Fields not submitted, display an error message
+echo "<br>" . "Please fill in all the required fields.";
+}
+
 
 ?>
