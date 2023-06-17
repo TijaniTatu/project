7ee99a60d4cc90c23dbd2b6ea83198da6516a9f5
TijaniTatu <riekotijani@gmail.com>
2023-06-18 02:25:08 +0300

    correction

--- posthomepage.php
+++ posthomepage.php
@@ -4,7 +4,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION["user_name"])) {
     // User is not logged in, redirect to login page
-    header("Location: login.html");
+    header("Location: loginform.html");
     exit();
 }
 
